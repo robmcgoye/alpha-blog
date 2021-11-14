51818e3a4ef49ffe2596855ea4ebffb9534b5ef7
Rob McGoye <rob@mcgoye.com>
2021-11-14 10:31:35 -0500

    Added articles model and crud operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
 end
