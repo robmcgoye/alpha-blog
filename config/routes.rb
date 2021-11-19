b566e495314ce38cb2d4e8804f19eeec9ce17322
Rob McGoye <rob@mcgoye.com>
2021-11-19 09:59:00 -0500

    Created user form and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
-  resources :articles
+  
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles
+  get 'signup', to: 'users#new'
+  resource :users, except: [:new]
+
 end
