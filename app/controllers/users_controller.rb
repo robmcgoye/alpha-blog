565f0c0f43a0b548b860008b23dc83573a537113
Rob McGoye <rob@mcgoye.com>
2021-11-20 07:25:04 -0500

    Created user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,14 @@ class UsersController < ApplicationController
     end 
   end 
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 80})
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravator_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravator_url, alt: user.username, class: "rounded shadow rounded mx-auto d-block")
+  end 
+
 end
